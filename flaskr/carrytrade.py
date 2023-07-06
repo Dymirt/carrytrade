68739d1f400eb5c51c3fa2016bb2ce1d7a21c09a
Dymirt <kolidadmytro@gmail.com>
2023-07-07 00:30:28 +0200

    from flask tutorials
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- flaskr/carrytrade.py
+++ flaskr/carrytrade.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask import render_template
+from .forex_data import forex_dict
 
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', forex_dict=forex_dict())
